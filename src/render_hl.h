b297bedf9536ee039b7e24ecc1b68056df7688ef
Paul Jarrett <jarrett.paul.young@gmail.com>
2020-01-04 09:49:22 -0500

    Adds docs for high level renderer.

--- src/render_hl.h
+++ src/render_hl.h
@@ -1,7 +1,17 @@
 /*
- * High-level render control.  Any sort of rendering backend should be able to
- * implement the functions defined here and be able to render the scene
- * appropriately.
+ * High-level render control.
+ *
+ * High-level control is the actual goals to accomplish during rendering, such
+ * as indicating frame start/end, clearing the screen, drawing a background, or
+ * drawing a sprite at a specific location.  The details of what exactly is
+ * happening is hidden from the game itself since the high-level control hides
+ * the details of how these details occur.
+ *
+ * Any sort of rendering backend should be able to implement the functions
+ * defined here and be able to render the scene appropriately.
+ *
+ * This engine supports 2D graphics only.  This includes sprites, lines,
+ * text, and polygons.
  */
 #ifndef KN_RENDER_HL_H
 #define KN_RENDER_HL_H
