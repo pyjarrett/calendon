607688974eae1b0e657998255708792b2e645d89
Paul Jarrett <jarrett.paul.young@gmail.com>
2020-01-30 23:06:28 -0500

    Split out animation looping into its own engine component.

--- src/demos/animation_loop.c
+++ src/demos/animation_loop.c
@@ -2,71 +2,13 @@
  * Working with animation loops.
  */
 #include <knell/kn.h>
+#include <knell/anim-loop.h>
 #include <knell/log.h>
 #include <knell/math2.h>
 #include <knell/render.h>
 #include <knell/time.h>
 
 LogHandle LogSysSample;
-
-typedef uint32_t AnimationStateId;
-
-#define KN_MAX_ANIMATION_STATES 16
-/*
- * Animation is split between the definition of the cycle itself, and individual
- * instances (cursors) of animation within that cycle.
- */
-typedef struct {
-	AnimationStateId current;
-	uint64_t elapsed;
-} AnimationLoopCursor;
-
-/**
- * Animations really don't care about what data they are accessing.  A timed
- * animation loop could access whatever data from an array, so the animation
- * state doesn't concern itself with that.
- */
-typedef struct {
-	// elapsed[i] is the amount of time spent in "i" before transitioning
-	uint64_t elapsed[KN_MAX_ANIMATION_STATES];
-	AnimationStateId numStates;
-} AnimationLoop;
-
-static AnimationStateId AnimLoop_NextState(AnimationLoop* loop, uint64_t current)
-{
-	KN_ASSERT(loop != NULL, "Cannot update a null animation loop");
-	return (AnimationStateId)((current + 1) % loop->numStates);
-}
-
-/**
- * Calculates the fraction of the transition which is complete for the cursor.
- */
-float AnimLoop_CalcAlpha(AnimationLoop* loop, AnimationLoopCursor* cursor)
-{
-	KN_ASSERT(loop != NULL, "Cannot update a null animation loop");
-	KN_ASSERT(cursor != NULL, "Cannot update a null animation cursor");
-
-	KN_ASSERT(cursor->current < loop->numStates, "Cursor %" PRIu32 " out of loop"
-		"state index bounds %" PRIu64, cursor->current, loop->numStates);
-
-	return  (float)cursor->elapsed / loop->elapsed[cursor->current];
-}
-
-/**
- * Using an animation loop, update the animation cursor to the next stage
- * if necessary.
- */
-void AnimLoop_Tick(AnimationLoop* loop, AnimationLoopCursor* cursor, uint64_t dt)
-{
-	KN_ASSERT(loop != NULL, "Cannot update a null animation loop");
-	KN_ASSERT(cursor != NULL, "Cannot update a null animation cursor");
-
-	cursor->elapsed += dt;
-	if (cursor->elapsed >= loop->elapsed[cursor->current]) {
-		cursor->current = AnimLoop_NextState(loop, cursor->current);
-		cursor->elapsed = 0;
-	}
-}
 
 #define SAMPLE_POINTS 4
 AnimationLoopCursor sampleCursor;
---
+++ src/knell/anim-loop.h
@@ -0,0 +1,48 @@
+#ifndef KN_ANIM_LOOP_H
+#define KN_ANIM_LOOP_H
+
+/*
+ * Animation loop controls.  Animations don't care about the underlying data
+ * types, so these could also be thought of as a FSM which loops through
+ * timed states.
+ */
+
+#include "kn.h"
+
+typedef uint32_t AnimationStateIndex;
+
+/**
+ * Bound the maximum number of states to something reasonable.
+ */
+#define KN_MAX_ANIMATION_STATES 16
+
+/*
+ * Animation is split between the definition of the cycle itself, and individual
+ * instances (cursors) of animation within that cycle.
+ *
+ * An animation cursor denotes the current state of a single instance of an
+ * animation.  This doesn't reference the AnimationLoop itself, to reduce the
+ * amount of state carried around, and also allows for cursors to be swapped
+ * to another loop.
+ */
+typedef struct {
+	AnimationStateIndex current;
+	uint64_t elapsed;
+} AnimationLoopCursor;
+
+/**
+ * Animations really don't care about what data they are accessing.  A timed
+ * animation loop could access whatever data from an array, so the animation
+ * state doesn't concern itself with that.
+ */
+typedef struct {
+	// elapsed[i] is the amount of time spent in "i" before transitioning
+	uint64_t elapsed[KN_MAX_ANIMATION_STATES];
+	AnimationStateIndex numStates;
+} AnimationLoop;
+
+KN_API float AnimLoop_CalcAlpha(AnimationLoop* loop, AnimationLoopCursor* cursor);
+KN_API AnimationStateIndex AnimLoop_NextState(AnimationLoop* loop, uint64_t current);
+KN_API void AnimLoop_Tick(AnimationLoop* loop, AnimationLoopCursor* cursor, uint64_t dt);
+
+#endif /* KN_ANIM_LOOP_H */
