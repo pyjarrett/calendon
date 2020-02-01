f3854a806a324419581926c752b45c89d4841434
Paul Jarrett <jarrett.paul.young@gmail.com>
2020-01-31 21:50:10 -0500

    Document control.h

--- src/knell/control.c
+++ src/knell/control.c
@@ -5,11 +5,18 @@
  */
 static bool running = true;
 
+/**
+ * Should continue ticking?
+ */
 KN_API bool Main_IsRunning(void)
 {
 	return running;
 }
 
+/**
+ * Tells the game to gracefully shutdown.  The program finishes the current
+ * tick before shutting down.
+ */
 KN_API void Main_QueueGracefulShutdown(void)
 {
 	running = false;
--- src/knell/control.h
+++ src/knell/control.h
@@ -1,20 +1,15 @@
-/*
- * Global program controls.
- */
 #ifndef KN_CONTROL_H
 #define KN_CONTROL_H
 
+/*
+ * Global program controls to initiate program shutdown.  Responsibility for
+ * queueing shutdown is separated from the main driver to prevent games from
+ * needing to link to anything outside of `knell-lib`.
+ */
+
 #include "kn.h"
 
-/**
- * Should continue ticking?
- */
 KN_API bool Main_IsRunning(void);
-
-/**
- * Tells the game to gracefully shutdown.
- */
 KN_API void Main_QueueGracefulShutdown(void);
 
 #endif /* KN_CONTROL_H */
-
