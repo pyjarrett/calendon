1f631204d14a5335d1cab83191b156f082c01c80
Paul Jarrett <jarrett.paul.young@gmail.com>
2020-01-31 21:43:12 -0500

    Document main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
+/*
+ * The main driver for the engine.  This driver loads the game library (.so or
+ * .dll) at runtime, allowing game and demo writers to focus on creating their
+ * demo, and not on redoing boilerplate.  In addition to saving time, this
+ * drastically reduces the complexity and amount of demo code, and compilation
+ * time.
+ *
+ * Since knell is a single-threaded engine, the driver starts up and shuts down
+ * the core game systems, runs the main loop, calculates the time delta since
+ * the last frame and executes the update and draw code provided by the game
+ * library.
+ *
+ * The game uses `Main_*` functions in `control_*` to ask the driver to
+ * shutdown.
+ */
+#include "knell/kn.h"
+
 #include "knell/assets.h"
 #include "knell/control.h"
 #include "knell/crash.h"
 #include "knell/env.h"
-#include "knell/kn.h"
 #include "knell/game.h"
 #include "knell/input.h"
 #include "knell/log.h"
@@ -204,6 +220,10 @@ bool Main_GenerateTick(uint64_t* outDt)
 	return true;
 }
 
+/**
+ * The big loop which processes events, ticks and draws until the game is ready
+ * to shut down.
+ */
 void Main_Loop(void)
 {
 	while (Main_IsRunning()) {
