dd2e3748406b4f635ce21c507aaf73123b52562c
Paul Jarrett <jarrett.paul.young@gmail.com>
2020-02-03 21:23:47 -0500

    Properly unflips images when loaded.

--- src/knell/image.c
+++ src/knell/image.c
@@ -1,10 +1,50 @@
 #include <knell/image.h>
 
+#include <knell/kn.h>
 #include <knell/assets-fileio.h>
 #include <knell/compat-spng.h>
 #include <knell/log.h>
 
 extern LogHandle LogSysAssets;
+
+void Image_Flip(ImagePixels* image)
+{
+	KN_ASSERT(image != NULL, "Cannot load flip a null image.");
+	KN_ASSERT(image->pixels.size > 0, "No pixels to flip.");
+	KN_ASSERT(image->width > 0, "Cannot flip an image with no width.");
+	KN_ASSERT(image->height > 0, "Cannot flip an image with no height.");
+
+	DynamicBuffer flippedImage;
+	Mem_Allocate(&flippedImage, image->pixels.size);
+
+	// Assume RGBA8 encoding.
+	const uint32_t pixelSize = 4 * sizeof(uint8_t);
+
+	const uint32_t expectedPixelStorageSize = pixelSize * image->width * image->height;
+	KN_ASSERT(expectedPixelStorageSize == image->pixels.size,
+		"Excessive storage for pixels found %" PRIu32 ", not matching resolution "
+		"%" PRIu32 "(%" PRIu32 ", %" PRIu32 ")", expectedPixelStorageSize,
+		image->pixels.size, image->width, image->height);
+
+	const uint32_t rowSize = pixelSize * image->width;
+
+	uint8_t* from = NULL;
+	uint8_t* to = NULL;
+
+	// Flip one row at a time.
+	for (uint32_t i = 0; i < image->height; ++i) {
+		from = (uint8_t*)flippedImage.contents + rowSize * (image->height - i - 1);
+		to = (uint8_t*)image->pixels.contents + rowSize * i;
+		memcpy(from, to, rowSize);
+	}
+
+	// Swap the buffer content pointers.
+	char* temp = image->pixels.contents;
+	image->pixels.contents = flippedImage.contents;
+	flippedImage.contents = temp;
+
+	Mem_Free(&flippedImage);
+}
 
 /**
  * Using `Allocate` as the name here to ensure the clients know to call
@@ -41,6 +81,8 @@ bool Image_Allocate(ImagePixels* image, const char* fileName)
 
 	image->width = header.width;
 	image->height = header.height;
+
+	Image_Flip(image);
 
 	KN_TRACE(LogSysAssets, "Loading image: %s", fileName);
 	KN_TRACE(LogSysAssets, "Image size %d, %d", header.width, header.height);
--- src/knell/image.h
+++ src/knell/image.h
@@ -7,13 +7,15 @@
 /**
  * Images are bulk groups of pixel data stored in memory and must be fed into
  * the renderer to create a texture to use for actual drawing.
+ *
+ * Images are assumed to be < 4GiB
  */
 typedef struct {
 	DynamicBuffer pixels;
 
 	/*
 	 * This imposes a limit of ~4 million pixels per side.  There probably
-	 * shouldn't be any levels even of that size.
+	 * shouldn't be any game levels even of that size.
 	 */
 	uint32_t width, height;
 } ImagePixels;
