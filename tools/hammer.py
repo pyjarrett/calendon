49913099351cf5227647ba9b789a6db4920433bb
Paul Jarrett <jarrett.paul.young@gmail.com>
2020-04-01 23:19:22 -0400

    Running hammer with args now runs the same commands as those the REPL.

--- tools/hammer.py
+++ tools/hammer.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 import importlib
 import kn
+import sys
 
 
 def reload_hammer():
     importlib.reload(kn)
 
 
-if __name__ == '__main__':
+def run_repl():
     while True:
         toll = kn.Hammer(reload_hammer)
         toll.cmdloop()
 
         if not toll.reload:
             break
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if len(args) == 0:
+        run_repl()
+    else:
+        toll = kn.Hammer(reload_hammer)
+        toll.onecmd(" ".join(args))
