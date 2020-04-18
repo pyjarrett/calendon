40d775daad715aa9f00326d8aa32e83489b912c8
Paul Jarrett <jarrett.paul.young@gmail.com>
2020-04-18 16:07:55 -0400

    Move ProjectContext into its own module.

--- py/knell/hammer.py
+++ py/knell/hammer.py
@@ -1,33 +1,22 @@
 """
 A Hammer context in which commands can be dispatched and executed.
 """
-from __future__ import annotations  # See PEP 484 and PEP 563.
 import argparse
 import cmd
-import copy
-from dataclasses import dataclass
-import dataclasses
-import json
 import multiprocessing
 import os
-from parsers import *
 import shutil
 import subprocess
 import sys
-from typing import Dict, Optional
+from typing import Optional
 
+from context import ProjectContext
+from parsers import *
 from run import run_program
 
 
 class Terminal(cmd.Cmd):
     pass
-
-
-def override_flavor_from_dict(flavor: object, kv: Dict):
-    """Overrides values in a flavor if they assigned in the namespace."""
-    for k in kv:
-        if kv[k] is not None and hasattr(flavor, k):
-            setattr(flavor, k, kv[k])
 
 
 def generator_settings_for_compiler(cmake_path: str, compiler_path: Optional[str]):
@@ -57,126 +46,6 @@ def generator_settings_for_compiler(cmake_path: str, compiler_path: Optional[str
             settings.extend(['-G', 'Unix Makefiles'])
 
     return settings
-
-
-@dataclass
-class ScriptFlavor:
-    knell_home: str = None
-    verbose: bool = False
-    dry_run: bool = False
-
-
-@dataclass
-class BuildFlavor:
-    """Define enough information to do a build."""
-    build_dir: str = 'build'
-    build_config: str = 'Debug'
-    compiler: str = None
-
-
-@dataclass
-class RunFlavor:
-    """Environment specification of how to do a 'run'."""
-    game: str = None
-    asset_dir: str = None
-    ticks: Optional[int] = 0
-    run_time_seconds: Optional[int] = 0
-    headless: bool = False
-
-
-class ProjectContext:
-    """A concise description of the environment in which the script will run."""
-
-    def __init__(self, knell_home: str):
-        """Creates a default context from a home directory."""
-        self._script_flavor = ScriptFlavor()
-        self._script_flavor.knell_home = os.path.abspath(knell_home)
-        self._build_flavor = BuildFlavor()
-        self._run_flavor = RunFlavor()
-        self._registered_programs = {}
-        self._load_config(os.path.join(self.knell_home(), '.hammer'))
-
-    def _override_from_dict(self, kv: Dict):
-        override_flavor_from_dict(self._script_flavor, kv)
-        override_flavor_from_dict(self._build_flavor, kv)
-        override_flavor_from_dict(self._run_flavor, kv)
-
-    def _load_config(self, config_path: str):
-        """Loads a config from a path."""
-        if not os.path.isfile(config_path):
-            print(f'No config file found at {config_path}')
-            return
-
-        with open(config_path, 'r') as file:
-            kv = json.load(file)
-            self._registered_programs = kv.get('registered_programs', {})
-            self._override_from_dict(kv)
-
-    def _save_config(self, config_path: str):
-        with open(config_path, 'w') as file:
-            combined = self.dump()
-            if self.is_verbose():
-                print(f'Saving to {config_path}')
-                print(combined)
-            json.dump(combined, file, indent=4)
-
-    def dump(self) -> Dict:
-        combined = {}
-        for flavor in [self._script_flavor, self._build_flavor, self._run_flavor]:
-            for field in dataclasses.fields(flavor):
-                combined[field.name] = getattr(flavor, field.name)
-        combined['registered_programs'] = self._registered_programs
-        return combined
-
-    def save(self):
-        self._save_config(os.path.join(self.knell_home(), '.hammer'))
-
-    def copy_with_overrides(self, kv: Dict) -> ProjectContext:
-        """Creates a new context with the given overrides applied."""
-        ctx = copy.deepcopy(self)
-        ctx._override_from_dict(kv)
-
-        if self._script_flavor.verbose:
-            print(f'Overrode: {self.__dict__}')
-            print(f'Using:    {kv}')
-            print(f'Result:   {self.__dict__}')
-        return ctx
-
-    def is_dry_run(self) -> bool:
-        return self._script_flavor.dry_run
-
-    def is_verbose(self) -> bool:
-        return self._script_flavor.verbose
-
-    def knell_home(self) -> str:
-        """Project root directory."""
-        return self._script_flavor.knell_home
-
-    def build_dir(self) -> str:
-        return os.path.abspath(os.path.join(self._script_flavor.knell_home, self._build_flavor.build_dir))
-
-    def build_config(self) -> str:
-        return self._build_flavor.build_config
-
-    def compiler(self) -> Optional[str]:
-        return self._build_flavor.compiler
-
-    def register_program(self, alias: str, path: str, override: bool = False) -> bool:
-        if alias in self._registered_programs and not override:
-            existing: str = self._registered_programs[alias]
-            print(f'Trying to override {existing} of {alias} with {path}')
-            return False
-
-        self._registered_programs[alias] = path
-        return True
-
-    def has_registered_program(self, alias: str) -> bool:
-        return alias in self._registered_programs
-
-    def path_for_program(self, alias: str) -> str:
-        if not self.has_registered_program(alias):
-            raise ValueError(f'No registered path for {alias}')
-        return self._registered_programs[alias]
 
 
 def verify_executable_exists(ctx: ProjectContext, alias: str) -> bool:
