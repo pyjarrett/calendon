dc1549d7694c42a79588fdb26f00cd8aef88e46a
Paul Jarrett <jarrett.paul.young@gmail.com>
2020-05-17 10:48:20 -0400

    Add checks and warning messages when Game_* functions doesn't exist in the game lib.

--- src/knell/game.c
+++ src/knell/game.c
@@ -24,9 +24,21 @@ KN_API void Game_Load(const char* sharedLibraryName)
 		KN_FATAL_ERROR("Unable to load game module: %s", sharedLibraryName);
 	}
 	Game_InitFn = (Game_InitPROC)GetProcAddress(GameModule, "Game_Init");
+	if (!Game_InitFn) {
+		KN_FATAL_ERROR("Game_Init function missing in %s", sharedLibraryName);
+	}
 	Game_DrawFn = (Game_DrawPROC)GetProcAddress(GameModule, "Game_Draw");
+	if (!Game_DrawFn) {
+		KN_FATAL_ERROR("Game_DrawFn function missing in %s", sharedLibraryName);
+	}
 	Game_TickFn = (Game_TickPROC)GetProcAddress(GameModule, "Game_Tick");
+	if (!Game_TickFn) {
+		KN_FATAL_ERROR("Game_TickFn function missing in %s", sharedLibraryName);
+	}
 	Game_ShutdownFn = (Game_ShutdownPROC)GetProcAddress(GameModule, "Game_Shutdown");
+	if (!Game_ShutdownFn) {
+		KN_FATAL_ERROR("Game_ShutdownFn function missing in %s", sharedLibraryName);
+	}
 
 	Game_InitFn();
 }
