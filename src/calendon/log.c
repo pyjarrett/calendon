04a4ba8adb4e157004a57433e828355fe83defda
Paul Jarrett <jarrett.paul.young@gmail.com>
2020-08-11 23:41:55 -0400

    Hooks up log disabling.

--- src/calendon/log.c
+++ src/calendon/log.c
@@ -121,7 +121,7 @@ void cnLog_RegisterSystem(uint32_t* system, const char* name, uint32_t verbosity
 
 void cnLog_Print(CnLogHandle system, CnLogVerbosity verbosity, const char* format, ...)
 {
-	if ((uint32_t)verbosity <= LogSystemsVerbosity[system]) {
+	if (s_config.enabled && (uint32_t)verbosity <= LogSystemsVerbosity[system]) {
 		va_list args;
 		va_start(args, format);
 		vprintf(format, args);
